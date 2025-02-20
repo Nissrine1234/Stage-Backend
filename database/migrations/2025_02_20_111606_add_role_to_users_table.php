2aeae2b38d032f2318f7369d948fe7c2e9575e9f
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-20 12:22:02 +0100

    Modification de la table users

---
+++ database/migrations/2025_02_20_111606_add_role_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role_type', ['admin', 'user_metier', 'fournisseur_morale', 'fournisseur_physique'])
+            ->nullable()
+            ->after('id');
+            $table->unsignedBigInteger('role_id')->nullable()->after('role_type');
+            $table->index(['role_type', 'role_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex(['role_type', 'role_id']);
+            $table->dropColumn('role_type');
+            $table->dropColumn('role_id');
+        });
+    }
+};
