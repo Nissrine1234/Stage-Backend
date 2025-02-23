c0ecc6e6bf6ffa6ad8ad293012f1ec56416956a0
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-23 15:18:07 +0000

    Update FournisseurPhysiqueService.php

--- app/Models/FournisseurPhysiqueService.php
+++ app/Models/FournisseurPhysiqueService.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FournisseurPhysiqueService extends Model
 {
     use HasFactory;
+
+    protected $table = 'fournisseur_physique_service';
+
 
     protected $fillable = [
         'service_achat_id',
