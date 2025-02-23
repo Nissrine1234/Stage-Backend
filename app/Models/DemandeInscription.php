27f42949286d36c02d0c41d78970a34435b4f6a5
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-23 15:20:28 +0000

    Update DemandeInscription.php

--- app/Models/DemandeInscription.php
+++ app/Models/DemandeInscription.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DemandeInscription extends Model
 {
     use HasFactory;
+
+    protected $table = 'demandes_inscriptions';
 
     protected $fillable = [
         'type_fournisseur',
