f4f2ce810e69598fbf1932819b6ef24a38580b95
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-23 15:15:47 +0000

    Update UserMetier.php

--- app/Models/UserMetier.php
+++ app/Models/UserMetier.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserMetier extends Model
 {
     use HasFactory;
+
+    protected $table = 'users_metiers';
 
     protected $fillable = [
         'user_id',
