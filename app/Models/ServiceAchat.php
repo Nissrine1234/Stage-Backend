a8d606a0130cf8107aae7146fe1d1580ed50d97d
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-23 15:16:49 +0000

    Update ServiceAchat.php

--- app/Models/ServiceAchat.php
+++ app/Models/ServiceAchat.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ServiceAchat extends Model
 {
     use HasFactory;
+
+    protected $table = 'service_achats';
 
     protected $fillable = [
         'nom_service',
