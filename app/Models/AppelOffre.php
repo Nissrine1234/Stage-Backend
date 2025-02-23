fc02ad6d412c38da0f35941bbc814c6e4b62f037
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-23 15:21:33 +0000

    Update AppelOffre.php

--- app/Models/AppelOffre.php
+++ app/Models/AppelOffre.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AppelOffre extends Model
 {
     use HasFactory;
+
+    protected $table = 'appels_offres';
 
     protected $fillable = [
         'titre',
