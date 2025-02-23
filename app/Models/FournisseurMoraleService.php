adeef1e20418aa2af7139f404451720880f00465
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-23 15:19:06 +0000

    Update FournisseurMoraleService.php

--- app/Models/FournisseurMoraleService.php
+++ app/Models/FournisseurMoraleService.php
@@ -2,14 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FournisseurMoraleService extends Model
 {
     use HasFactory;
 
+    protected $table = 'fournisseur_morale_service';
+    
     protected $fillable = [
         'service_achat_id',
         'fournisseur_morale_id',
     ];
 }
+
