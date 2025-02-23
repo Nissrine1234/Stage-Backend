960de4b4068d49834417a7f45cecc8fed9aa4ab7
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-23 15:21:06 +0000

    Update AppelOffreService.php

--- app/Models/AppelOffreService.php
+++ app/Models/AppelOffreService.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AppelOffreService extends Model
 {
     use HasFactory;
+
+    protected $table = 'appel_offre_service';
 
     protected $fillable = [
         'service_achat_id',
