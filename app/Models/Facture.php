64430c1ee19ba43403574bdf8a0d4f9c7493ddc6
Nissrine1234 <150920945+Nissrine1234@users.noreply.github.com>
2025-02-23 15:20:09 +0000

    Update Facture.php

--- app/Models/Facture.php
+++ app/Models/Facture.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Facture extends Model
 {
     use HasFactory;
+
+    protected $table = 'factures';
 
     protected $fillable = [
         'demande_id',
